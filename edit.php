2d19e788641adb42c8d0b887b181961914284048
hatakumi323 <takuken135@gmail.com>
2021-09-19 18:40:24 +0900

    overwrite

--- edit.php
+++ edit.php
@@ -5,24 +5,51 @@ include 'lib/connect.php';
 include 'lib/queryArticle.php';
 include 'lib/article.php';
 
-$title = '';
-$body = '';
-$title_alert = '';
-$body_alert = '';
+$title = "";        // タイトル
+$body = "";         // 本文
+$id = "";           // ID
+$title_alert = "";  // タイトルのエラー文言
+$body_alert = "";   // 本文のエラー文言
 
-if (!empty($_POST['title']) && !empty($_POST['body'])) {
-  // titleとbodyがPOSTメソッドで送信されたとき
+if (isset($_GET['id'])) {
+  $queryArticle = new QueryArticle();
+  $article = $queryArticle->find($_GET['id']);
+
+  if ($article) {
+    // 編集する記事データが存在したとき、フォームに埋め込む
+    $id = $article->getId();
+    $title = $article->getTitle();
+    $body = $article->getBody();
+  } else {
+    // 編集する記事データが存在しないとき
+    header('Location: backend.php');
+    exit;
+  }
+} else if (!empty($_POST['id']) && !empty($_POST['title']) && !empty($_POST['body'])) {
+  // id, titleとbodyがPOSTメソッドで送信されたとき
   $title = $_POST['title'];
   $body = $_POST['body'];
 
-  $article = new Article();
-  $article->setTitle($title);
-  $article->setBody($body);
-  $article->save();
-
+  $queryArticle = new QueryArticle();
+  $article = $queryArticle->find($_POST['id']);
+  if ($article) {
+    // 記事データが存在していれば、タイトルと本文を変更して上書き保存
+    $article->setTitle($title);
+    $article->setBody($body);
+    $article->save();
+  }
   header('Location: backend.php');
+  exit;
 } else if (!empty($_POST)) {
   // POSTメソッドで送信されたが、titleかbodyが足りないとき
+  if (!empty($_POST['id'])) {
+    $id = $_POST['id'];
+  } else {
+    // 編集する記事IDがセットされていなければ、backend.phpへ戻る
+    header('Location: backend.php');
+    exit;
+  }
+
   // 存在するほうは変数へ、ない場合空文字にしてフォームのvalueに設定する
   if (!empty($_POST['title'])) {
     $title = $_POST['title'];
@@ -38,6 +65,7 @@ if (!empty($_POST['title']) && !empty($_POST['body'])) {
 }
 
 ?>
+
 <!doctype html>
 <html lang="ja">
 
@@ -85,7 +113,10 @@ if (!empty($_POST['title']) && !empty($_POST['body'])) {
     <div class="row">
       <div class="col-md-12">
 
-        <form action="post.php" method="post">
+        <h1>記事の編集</h1>
+
+        <form action="edit.php" method="post">
+          <input type="hidden" name="id" value="<?php echo $id ?>">
           <div class="mb-3">
             <label class="form-label">タイトル</label>
             <?php echo !empty($title_alert) ? '<div class="alert alert-danger">' . $title_alert . '</div>' : '' ?>
